ea51865e083ffb572568068e9913a4fd27de7473
Okiljon Rakhimov <58033763+00008536@users.noreply.github.com>
2021-11-19 14:16:05 +0500

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vip70
+{
+    public class Car
+    {
+       
+
+        private int Id { get; set; }
+        private string Type { get; set; }
+        private float Price { get; set; }
+
+        private string Model { get; set; }
+
+        private string Brand { get; set; }
+        private string Condition { get; set; }
+        private int Year { get; set; }
+
+        public Car(int id, string type, float price, string model, string brand, string condition, int year)
+        {
+            this.Id = id;
+             this.Type = type;
+             this.Price = price;
+             this.Model = model;
+             this.Brand = brand;
+             this.Condition = condition;
+            this.Year = year;
+        }
+
+        public string GetCarType()
+        {
+            return this.Type;
+        }
+
+        public string GetCarModel()
+        {
+            return this.Model;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id.ToString()} | {Price.ToString("C")} | {Model} | {Condition} | {Brand} | {Year.ToString()}";
+        }
+    }
+}
---
+++ CarList.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vip70
+{
+    public class CarList
+    {
+        private static List<Car> Cars = new List<Car>();
+
+        public void AddCar(int Id, string Type, float Price, string Model, string Brand, string Condition, int Year)
+        {
+            Cars.Add(new Car(Id, Type, Price, Model, Brand, Condition, Year));
+            Console.WriteLine("Adding the Car" + Model);
+        }
+
+        public List<Car> GetAllCars()
+        {
+            return Cars;
+        }
+
+        public List<Car> GetIndustrialCars()
+        {
+            List<Car> industrialCars = new List<Car>();
+            foreach(Car car in Cars)
+            {
+                if (car.GetCarType() == "Industrial")
+                {
+                    industrialCars.Add(car);
+
+                }
+            }
+            return industrialCars;
+        }
+
+        public List<Car> GetPassengerCars()
+        {
+            List<Car> passengerCars = new List<Car>();
+            foreach(Car car in Cars)
+            {
+                if(car.GetCarType() == "Passenger")
+                {
+                    passengerCars.Add(car);
+                }
+            }
+            return passengerCars;
+        }
+
+        public List<Car> GetCarsByModel(string model)
+        {
+            List<Car> carsByModel = new List<Car>();
+            foreach (Car car in Cars)
+            {
+                if (car.GetCarModel() == model)
+                {
+                    carsByModel.Add(car);
+                }
+            }
+            return carsByModel;
+        }
+
+        public List<Car> SearchCarsByModel(string model)
+        {
+            List<Car> carsByModel = new List<Car>();
+            foreach (Car car in Cars)
+            {
+                if (car.GetCarModel().Contains(model))
+                {
+                    carsByModel.Add(car);
+                }
+            }
+            return carsByModel;
+        }
+
+        public List<Car> SearchCarsByIndustry(string industry)
+        {
+            switch (industry)
+            {
+                case "Passenger":
+                case "passenger":
+                    return GetPassengerCars();
+                case "Industrial":
+                case "industrial":
+                    return GetIndustrialCars();
+                default:
+                    return GetAllCars();
+            }
+        }
+
+        public List<Car> GetCarsByArguments(string model = "", string industry = "")
+        {
+            if (string.IsNullOrWhiteSpace(model) && !string.IsNullOrWhiteSpace(industry))
+            {
+                return SearchCarsByIndustry(industry);
+            }
+            else if (!string.IsNullOrWhiteSpace(model) && string.IsNullOrWhiteSpace(industry))
+            {
+                return SearchCarsByModel(model);
+            }
+            else if (!string.IsNullOrWhiteSpace(model) && !string.IsNullOrWhiteSpace(industry))
+            {
+                return SearchCarsByModel(model).Intersect(SearchCarsByIndustry(industry)).ToList();
+            }
+            else
+            {
+                return GetAllCars();
+            }
+        }
+
+        public void DisplayCars()
+        {
+            foreach (Car car in Cars)
+            {
+                Console.WriteLine(car.ToString());
+            }
+        }
+
+    }
+}
